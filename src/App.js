4e8be0f74281dcb8da8e6d10463f9f74d22b443a
Maria Bolaños <mariadlp@gmail.com>
2019-05-29 12:05:39 -0500

    Firebase has been installed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from './firebase/start-firebase';
+
+
 
 function App() {
   return (
---
+++ src/firebase/start-firebase.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDsEvWbt-yS5QTwLknPDz4foXZBpcmWvX0",
+    authDomain: "organalaboratoria.firebaseapp.com",
+    databaseURL: "https://organalaboratoria.firebaseio.com",
+    projectId: "organalaboratoria",
+    storageBucket: "organalaboratoria.appspot.com",
+    messagingSenderId: "349628877024",
+    appId: "1:349628877024:web:30c5a92277076304"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  export default firebase;
+
+
