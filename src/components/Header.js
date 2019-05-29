afd64338f924d55d739840d55ef4ea6759cbc03e
maribel-arac <maribel.arca@gmail.com>
2019-05-29 18:29:05 -0500

    ShowDate component is included

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,17 +5,22 @@ class Header extends Component {
 	render () {
 
 		return (
+			<div className="container">
+				<div className="header">
+					<nav className="navbar">
+						<div className="navbar-brand">
+							<img className="logo img-fluid" src={require("../images/logo.png" )}/>
+						</div>
+					</nav>
 
-			<div className="header">
-				<nav className="navbar">
-					<div className="navbar-brand">
-						<img className="logo img-fluid" src={require("../images/logo.png" )}/>
-					</div>
-				</nav>
+				</div>
 
 			</div>
 
-		)
+			<div>
+			<ShowDate />
+			</div>
+		);
 
 	}
 
