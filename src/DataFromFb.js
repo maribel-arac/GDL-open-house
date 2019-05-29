8f1528470717824521847b98efd6e3a72d492980
Maria Bolaños <mariadlp@gmail.com>
2019-05-29 13:18:42 -0500

    Integrated firebase with react

---
+++ src/DataFromFb.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import './App.css';
+import firebase from './firebase/start-firebase';
+
+export class DataFromFb extends Component {
+
+	constructor(){
+		super();
+		this.state = {
+			students: []
+		};
+	}
+
+
+	componentDidMount() {
+
+		const studentsRef = firebase.database().ref('students-ref');
+
+		studentsRef.on('value', (snapshot) => {
+
+			let students = snapshot.val();
+
+			let studentsUpdate = [];
+
+			for (let student in students) {
+
+				studentsUpdate.push({
+					id: student.id,
+					name: student.name,
+					mail: student.mail,
+					code: student.code,
+					active: student.active
+				});
+
+			}
+
+			this.setState({
+				students: studentsUpdate
+			});
+
+		});
+
+	}
+
+
+
+
+}
+
+export default DataFromFb;
