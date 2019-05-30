ed815704cf562e8ba9799748a595de6a0763ee7b
Maria Bolaños <mariadlp@gmail.com>
2019-05-29 19:50:35 -0500

    Menu Icon displays in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,16 +7,41 @@ class Header extends Component {
 
 		return (
 
-				<div className="header">
+			<div className="header">
+				<div class="row">
 
 					<nav className="navbar">
-						<div className="navbar-brand" >
-							<img className="logo img-fluid" alt="logo" src={require("../images/logo.png" )}/>
+						<div class="col-sm-5">
+							<div className="navbar-brand" >
+								<img className="logo img-fluid" alt="logo" src={require("../images/logo.png" )}/>
+							</div>
 						</div>
-						<ShowDate />
-					</nav>
 
+						<div class="col-sm-5">
+							<ShowDate />
+						</div>
+
+						<div class="col-sm-2">
+
+							<div class="nav-item dropdown">
+								<a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									<i class="fas fa-bars"></i>
+								</a>
+
+								<div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+									<a className="dropdown-item" href="#">Inicio</a>
+									<a className="dropdown-item" href="#">Toma de Asistencia</a>
+									<a className="dropdown-item" href="#">Historial</a>
+								</div>
+
+							</div>
+
+						</div>
+
+					</nav>
 				</div>
+
+			</div>
 
 
 		);
