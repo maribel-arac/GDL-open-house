a6daf48b9fd26bf6934b87bf4de5f9c3f6994127
Maria Bolaños <mariadlp@gmail.com>
2019-05-30 14:26:16 -0500

    Created component History for displaying charts and data when generated

---
+++ src/components/History.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class History extends Component {
+
+	render () {
+
+		return (
+
+			<div className="history"></div>
+
+		);
+
+	}
+
+}
+
+export default History;
